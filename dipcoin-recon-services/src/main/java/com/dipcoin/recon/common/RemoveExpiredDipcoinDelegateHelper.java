82a3510ad28f21a7b073a0d39a0816a04283abfc
tushartari11 <tushartari11@gmail.com>
2016-02-26 20:25:01 +0530

    Remaining File

--- dipcoin-recon-services/src/main/java/com/dipcoin/recon/common/RemoveExpiredDipcoinDelegateHelper.java
+++ dipcoin-recon-services/src/main/java/com/dipcoin/recon/common/RemoveExpiredDipcoinDelegateHelper.java
@@ -1,8 +1,12 @@
 package com.dipcoin.recon.common;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ThreadPoolExecutor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,6 +21,9 @@ import com.dipcoin.db.services.model.Dipcoin;
 public class RemoveExpiredDipcoinDelegateHelper implements
 		RemoveExpiredDipcoinDelegate {
 
+	private static final int INITIAL_PAGE_INDEX = 0;
+	private static final int NO_OF_RECORDS_PER_PAGE = 100;
+	
 	@Autowired
 	DipcoinDBService coinDBService;
 
@@ -32,11 +39,23 @@ public class RemoveExpiredDipcoinDelegateHelper implements
 	@Override
 	public boolean removeExpiredDipcoins() {
 		try {
-			Future<List<Dipcoin>> asyncResults = coinDBService
-					.getExpiredDipcoins();
-			List<Dipcoin> expiredDipcoins = asyncResults.get();
-			for (Dipcoin dipcoin : expiredDipcoins) {
-				createAndFireTransactionsForLienRemoval(dipcoin);
+			int startIndex = INITIAL_PAGE_INDEX, pageSize = NO_OF_RECORDS_PER_PAGE;
+			while (true) {
+				List<Future<Dipcoin>> resultList = new ArrayList<>();
+				Future<List<Dipcoin>> asyncResults = coinDBService.getExpiredDipcoins(pageSize, startIndex);
+				List<Dipcoin> expiredDipcoins = asyncResults.get();
+				if (expiredDipcoins == null || expiredDipcoins.isEmpty())
+					break;
+				ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors
+						.newFixedThreadPool(expiredDipcoins.size()); // need to check on the number of threads
+				for (Dipcoin dipcoin : expiredDipcoins) { 
+					LienRemovalHelper lienRemovalHelper = new LienRemovalHelper(dipcoin);
+					Future<Dipcoin> result = executor.submit(lienRemovalHelper);
+					resultList.add(result);
+				}
+				startIndex += pageSize;
+				//shut down the executor service when the FutureTasks are completed
+	            executor.shutdown();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -49,29 +68,50 @@ public class RemoveExpiredDipcoinDelegateHelper implements
 	 * DipCoin and creates a transaction to be sent <b>for Lien Removal</b> to
 	 * the Bank Services
 	 * 
-	 * @param dipcoin theDipcoin which is expired
+	 * @param dipcoin
+	 *            theDipcoin which is expired
 	 */
+	
+	class LienRemovalHelper implements Callable<Dipcoin>{
 
-	private void createAndFireTransactionsForLienRemoval(Dipcoin dipcoin) {
-		BankTransaction bankTransaction = new BankTransaction();
-		bankTransaction.setAmount(dipcoin.getAmount());
-		bankTransaction.setDipcoinTransaction(dipcoin.getDipcoinTransaction());
-		bankTransaction.setDipcoin(dipcoin);
-		try {
-			Future<BankTransaction> asyncResult = bankDBService.createTransaction(bankTransaction);
-			BankTransaction createdTransaction = asyncResult.get();
-
-			BankTransaction resultedTransaction = lienRemovalDelegate.removeLien(createdTransaction);
-			bankDBService.updateBankTransactionStatus(resultedTransaction);
-
-			dipcoin.setStatusWithBank(resultedTransaction.getStatus()); // set the statuswithbank flag in dipcoin
-			resultedTransaction.setDipcoin(dipcoin);
-			// Update the Dipcoin Status with Dipcoin with the status received from the bank
-			coinDBService.updateCoin(resultedTransaction.getDipcoin(),resultedTransaction.getStatus());
-
-		} catch (InterruptedException | ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		private Dipcoin dipcoin;
+		 
+	    public LienRemovalHelper(Dipcoin dipcoin) {
+	        this.dipcoin = dipcoin;
+	    }
+	    
+		@Override
+		public Dipcoin call() throws Exception {
+			Dipcoin updateDipcoin = createAndFireTransactionsForLienRemoval(dipcoin);
+			return updateDipcoin;
 		}
+		
+		private Dipcoin createAndFireTransactionsForLienRemoval(Dipcoin dipcoin) {
+			BankTransaction bankTransaction = new BankTransaction();
+			Dipcoin updatedDipcoin  = null;
+			bankTransaction.setAmount(dipcoin.getAmount());
+			bankTransaction.setDipcoinTransaction(dipcoin.getDipcoinTransaction());
+			bankTransaction.setDipcoin(dipcoin);
+			try {
+				Future<BankTransaction> asyncResult = bankDBService.createTransaction(bankTransaction);
+				BankTransaction createdTransaction = asyncResult.get();
+
+				BankTransaction resultedTransaction = lienRemovalDelegate.removeLien(createdTransaction);
+				bankDBService.updateBankTransactionStatus(resultedTransaction);
+
+				dipcoin.setStatusWithBank(resultedTransaction.getStatus()); // set the statuswithbank flag in dipcoin
+				resultedTransaction.setDipcoin(dipcoin);
+				// Update the Dipcoin Status with Dipcoin with the status received
+				// from the bank
+				 updatedDipcoin = coinDBService.updateCoin(resultedTransaction.getDipcoin(),
+						resultedTransaction.getStatus()).get();
+
+			} catch (InterruptedException | ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return updatedDipcoin;
+		}
+		
 	}
 }
